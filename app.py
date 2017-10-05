bab27a3f74884106882ce5092956afd059041ede
anish2454 <shenoy.anish.227@gmail.com>
2017-10-04 20:25:29 -0400

    Updated templates

--- app.py
+++ app.py
@@ -12,21 +12,21 @@ import os
 
 app = Flask(__name__)
 
+#genrerates random key
 app.secret_key = os.urandom(32)
 
 @app.route('/')
 def home():
-    if session.has_key('username') == True: #renders welcome page if a 'username' key exists
-        return render_template('welcome.html',UN = session['username'])
+    if session.has_key('username'): #renders welcome page if a 'username' key exists
+        return render_template('welcome.html', UN = session['username']) #session['username'] is safe b/c we checked for its existance
     return render_template('login.html') #otherwise renders the login page
 
 @app.route('/login', methods=['POST']) #uses POST method for form submission
 def login():
-    if request.form['username'] == 'admin': #the only correct username is "admin"
-        if request.form['password'] == 'password': #the only correct password is "password"
-            session['username'] = 'admin' #sets the 'username' key to something so that it can be recognized
-            return render_template('login_success.html') #tells the user they are logged in
-    return render_template('login_fail.html') #otherwise tells the user their credentials were incorrect
+    if request.form['username'] == 'admin' and request.form['password'] == 'password': #the only correct username is "admin" and the only correct password is "password"
+        session['username'] = 'admin' #sets the 'username' key to something so that it can be recognized
+        return render_template('logged.html') #tells the user they are logged in
+    return render_template('failed.html') #otherwise tells the user their credentials were incorrect
 
 @app.route('/logout')
 def logout():
